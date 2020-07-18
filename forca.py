437bdab101eafe10dcfa80182a6ba0f40f71e034
Anna Carolina <ac.luna95@gmail.com>
2020-07-18 19:01:30 -0400

    Add initial forca game

---
+++ forca.py
@@ -0,0 +1,8 @@
+
+def jogar_forca():
+    print ("*********************************")
+    print ("***Bem vindo no jogo de Forca!***")
+    print ("*********************************")
+
+
+    print ("Fim de Jogo!")
